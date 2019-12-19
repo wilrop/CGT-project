05b5bccc8ca330d0e68247528be4f41ff30a804b
Willem Röpke <willem.ropke@vub.be>
2019-12-19 14:04:57 +0100

    Documentation updates

--- Game.py
+++ Game.py
@@ -1,4 +1,7 @@
 class Game:
+    """
+    A class that represents a collective-risk dilemma as an evolutionary game.
+    """
     def __init__(self, players, risk, rounds):
         self.players = players
         self.risk = risk
--- Player.py
+++ Player.py
@@ -1,4 +1,7 @@
 class Player:
+    """
+    A class that represents a player in our collective-risk dilemma.
+    """
     def __init__(self, coins):
         self.balance = coins
 
