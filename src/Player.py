0527c78a8117b82068d2aed2c0907d04c8a5c3a9
Willem Röpke <willem.ropke@vub.be>
2019-12-19 14:40:48 +0100

    Added a player strategy

--- src/Player.py
+++ src/Player.py
@@ -1,9 +1,17 @@
+import numpy as np
+
 class Player:
     """
     A class that represents a player in our collective-risk dilemma.
     """
-    def __init__(self, coins):
+    def __init__(self, coins, legal_moves):
         self.balance = coins
+        self.legal_moves = legal_moves  # The contributions that are allowed in our game.
+
+        # The player strategy.
+        self.threshold = np.random.uniform(0, 1)  # Initial thresholds are uniformly distributed between 0 and 1.
+        self.strategy_above = np.random.choice(legal_moves)
+        self.strategy_below = np.random.choice(legal_moves)
 
     def choose_action(self):
         return 0
