13b18bdb1345d7698abec801468961d5a4a1b0a6
Willem Röpke <willemropke@hotmail.com>
2019-12-24 11:59:41 +0100

    Created a class that contains all variables that will be used in the simulation

---
+++ src/BuildSimulation.py
@@ -0,0 +1,26 @@
+class BuildSimulation:
+    """
+    A class that holds all variables that will be used in the simulation. This class will get passed as a parameter and
+    different components of the simulation will use different variables from it.
+    """
+    def __init__(self):
+        # Variables that define an amount for something.
+        self.num_generations = 10 ** 4
+        self.num_games = 10 ** 3
+        self.num_rounds = 10
+        self.num_players = 6
+
+        # Variables for the players
+        self.population_size = 100
+        self.initial_endowment = 2 * self.num_rounds
+        self.legal_moves = [0, 1, 2]
+
+        # The risk that a player loses their balance.
+        self.risk = 0.5  # Risk is represented as a probability.
+
+        # A measure for the intensity of selection
+        self.beta = 1
+
+        # The variables used in the noise generation process when evolving a generation.
+        self.mu = 0.15
+        self.sigma = 0
