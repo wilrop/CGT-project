ee9af80359afe797573e3015efa5b459d9d2c65c
Willem Röpke <willemropke@hotmail.com>
2019-12-24 12:01:30 +0100

    Added the game playing mechanics

--- src/Game.py
+++ src/Game.py
@@ -1,17 +1,36 @@
+import numpy as np
+
+
 class Game:
     """
     A class that represents a collective-risk dilemma as an evolutionary game.
     """
-    def __init__(self, players, num_rounds, risk):
+    def __init__(self, setup, players):
         self.players = players
-        self.num_rounds = num_rounds
-        self.risk = risk
+        self.num_rounds = setup.num_rounds
+        self.risk = setup.risk
 
-        self.target_sum = len(players) * num_rounds  # The target sum that needs to be invested before succeeding.
+        self.target_sum = len(players) * self.num_rounds  # The target sum that needs to be invested before succeeding.
 
     def play(self):
         """
         A method that will play a single game.
         :return:
         """
-        return 0
+        contributions = 0
+        for round in range(self.num_rounds):
+            for player in self.players:
+                if contributions < player.threshold:
+                    contributions += player.strategy_below
+                else:
+                    contributions += player.strategy_above
+
+        # Give The payoffs after playing the game.
+        if contributions >= self.target_sum or self.risk < np.random.uniform(0, 1):
+            for player in self.players:
+                player.payoffs.append(player.balance)
+        else:
+            for player in self.players:
+                player.payoffs.append(0)
+
+        return contributions
