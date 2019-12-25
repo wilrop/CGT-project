27da56f81b590278a7ecddcd01c56c4278fc46d6
Willem Röpke <willemropke@hotmail.com>
2019-12-25 01:23:39 +0100

    Improved on the evolutionary dynamics

--- src/Generation.py
+++ src/Generation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 from src.Game import Game
 from src.Player import Player
@@ -13,7 +14,8 @@ class Generation:
         self.num_games = setup.num_games
         self.num_players = setup.num_players
         self.num_rounds = setup.num_rounds
-        self.population = [Player(setup.initial_endowment, setup.legal_moves) for x in range(setup.population_size)]
+        self.population_size = setup.population_size
+        self.population = [Player(setup.initial_endowment, setup.legal_moves, self.num_rounds) for x in range(self.population_size)]
         self.risk = setup.risk
         self.beta = setup.beta
 
@@ -23,7 +25,7 @@ class Generation:
         :return: Nothing as of now. TODO make it return something useful.
         """
         for i in range(self.num_games):
-            players = np.random.choice(self.population, self.num_players)
+            players = np.random.choice(self.population, self.num_players, replace=False)
             game = Game(self.setup, players)
             game.play()
         return 0
@@ -35,6 +37,7 @@ class Generation:
         """
         avg_payoffs = [np.average(player.payoffs) for player in self.population]
         fitness = np.exp(avg_payoffs * self.beta)
+        print(np.average(avg_payoffs))
         return fitness
 
     def evolve(self):
@@ -42,5 +45,14 @@ class Generation:
         A function that will execute a Wright-Fisher process to evolve a generation.
         :return: /
         """
-        new_population = self.population
-        self.population = new_population
+        fi_lst = self.calculate_fitness()
+        fitness_sum = np.sum(fi_lst)
+        probabilities = [fi / fitness_sum for fi in fi_lst]
+
+        parents = np.random.choice(self.population, size=self.population_size, replace=True, p=probabilities)
+        offspring = []
+        for parent in parents:
+            child = parent.create_offspring(self.setup.mu, self.setup.sigma)
+            offspring.append(child)
+
+        self.population = offspring
--- src/Player.py
+++ src/Player.py
@@ -5,14 +5,60 @@ class Player:
     """
     A class that represents a player in our collective-risk dilemma.
     """
-    def __init__(self, coins, legal_moves):
+    def __init__(self, coins, legal_moves, rounds):
+        self.starting_balance = coins
         self.balance = coins
         self.legal_moves = legal_moves  # The contributions that are allowed in our game.
+        self.rounds = rounds
 
         # The player strategy.
-        self.threshold = np.random.uniform(0, 1)  # Initial thresholds are uniformly distributed between 0 and 1.
-        self.strategy_above = np.random.choice(legal_moves)
-        self.strategy_below = np.random.choice(legal_moves)
+        self.thresholds = [np.random.uniform(0, 1) for i in range(rounds)]
+        self.strategies_above = [np.random.choice(legal_moves) for i in range(rounds)]
+        self.strategies_below = [np.random.choice(legal_moves) for i in range(rounds)]
 
         # Initialize the player's payoff history
         self.payoffs = []
+
+    def select_action(self, round, contributions, target):
+        threshold = self.thresholds[round] * target
+        strategy_above = self.strategies_above[round]
+        strategy_below = self.strategies_below[round]
+
+        if contributions < threshold and self.balance >= strategy_below:
+            self.balance -= strategy_below
+            return strategy_below
+        elif contributions >= threshold and self.balance >= strategy_above:
+            self.balance -= strategy_above
+            return strategy_above
+
+    def create_offspring(self, mu, sigma):
+        """
+        A method that will create a child from this player.
+        :param mu: The mutation probability.
+        :param sigma: The standard deviation in the normal distribution used for the noise generation.
+        :return: A newly created player that represents the child of this player.
+        """
+        def calc_noise():
+            """
+            Calculate noise from a normal distribution with a given standard deviation.
+            :return: The random sample from the distribution.
+            """
+            return np.random.normal(scale=sigma)
+
+        offspring = Player(self.starting_balance, self.legal_moves, self.rounds)
+        offspring.thresholds = self.thresholds
+        offspring.strategy_above = self.strategies_above
+        offspring.strategy_below = self.strategies_below
+
+        # Check for random mutation in every round.
+        for round in range(self.rounds):
+            if np.random.uniform(0, 1) < mu:
+                offspring.thresholds[round] += calc_noise()
+
+            if np.random.uniform(0, 1) < mu:
+                offspring.strategies_above[round] = np.random.choice(self.legal_moves)
+
+            if np.random.uniform(0, 1) < mu:
+                offspring.strategies_below[round] = np.random.choice(self.legal_moves)
+
+        return offspring
