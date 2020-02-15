b9e4f38267bfccff1f7a9709577df486ecde2944
Willem Röpke <willem.ropke@vub.be>
2020-02-15 22:31:24 +0100

    Bugfix

--- src/Generation.py
+++ src/Generation.py
@@ -72,7 +72,7 @@ class Generation:
 
             # Check the frequency of the strategy if we are looking for it.
             if self.strategy is not None:
-                if self.strategy == child.strategy.tolist():
+                if self.strategy == child.strategy:
                     self.frequency += 1
 
             offspring.append(child)
